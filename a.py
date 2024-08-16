43d582a5ba3acdb7488c14761b6e789f7220abef
Armaan-jay <armaanch2004@gmail.com>
2024-08-16 19:56:03 +0530

    integer declared

--- a.py
+++ a.py
@@ -1,1 +1,5 @@
 print("HELO NITESH BHAIYA")
+#git add . = is a command in which we wan to push all the files to the github account
+#git commit -m=  is used to to add file into the git hub account
+#git push=used to push the files into github
+a=10
